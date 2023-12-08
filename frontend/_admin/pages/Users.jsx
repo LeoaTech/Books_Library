1988b6cf436d5b3a771f4bde7a669f255fdf0187
razaakomal <komal.raza@leoatech.com>
2023-12-08 10:51:19 +0500

    LIB-21 Create User Page and Table

---
+++ frontend/_admin/pages/Users.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { UserTable } from '../../components/_admin'
+
+const Users = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">
+        Users
+      </h1>
+      <UserTable />
+    </div>
+  )
+}
+
+export default Users
