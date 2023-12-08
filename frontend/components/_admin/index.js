17286e93cbe2be322d724de9aa058e2c92d8bf58
razaakomal <komal.raza@leoatech.com>
2023-12-08 11:00:38 +0500

    Exported All Components

---
+++ frontend/components/_admin/index.js
@@ -0,0 +1,21 @@
+export { default as Notification } from "./Account/Notification";
+export { default as Profile } from "./Account/ProfileComponent";
+export { default as Settings } from "./Account/Settings";
+export { default as DropdownProfileMenu } from "./Account/DropDownProfileMenu";
+
+export { default as DropdownNotification } from "./Navbar/DropdownNotification";
+export { default as Header } from "./Navbar/Header";
+export { default as NotificationCard } from "./Account/NotificationCard";
+
+export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as SidebarLinksGroup } from "./Sidebar/SidebarLinkGroup";
+
+export { default as BooksTable } from "./ui/Tables/BooksTable";
+export { default as UserTable } from "./ui/Tables/UserTable";
+export { default as Table } from "./ui/Tables/Tables";
+export { default as OrdersTable } from "./ui/Tables/OrderTable";
+export { default as ShippingsTable } from "./ui/Tables/ShippingDetailsTable";
+export { default as ReturnsTable } from "./ui/Tables/ReturnsTable";
+export { default as BookingsTable } from "./ui/Tables/BookingTable";
+
+export { default as ShippingTabs } from "./ui/Tabs/ShippingTabs";
